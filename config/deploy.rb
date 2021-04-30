c7d89ea3f806cae977d3f115910392a9da57bd18
shotaro sakai <dt.sakaser.hokuoh100@gmail.com>
2021-04-30 12:28:03 +0900

    deploy.rbをの編集②

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock 3.16.0
 set :application, first-35242
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:Githubのユーザー名/レポジトリ名.git'
+set :repo_url,  https://github.com/putaro1222/first-35242.git
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
