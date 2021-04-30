afee3c00e2ee0a3e5c59675d05820dbdaab19ebe
shotaro sakai <dt.sakaser.hokuoh100@gmail.com>
2021-04-30 14:51:58 +0900

    トップページの仮実装

---
+++ app/controllers/lectures_controller.rb
@@ -0,0 +1,5 @@
+class LecturesController < ApplicationController
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
+  root to: 'lectures#index'
  end
