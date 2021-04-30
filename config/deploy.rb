a11e91fc63d1747f50c3d10082b6c7553584c661
shotaro sakai <dt.sakaser.hokuoh100@gmail.com>
2021-04-30 14:16:12 +0900

    deploy.rbとproduction.rbの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,17 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock 3.16.0
+lock '3.16.0'
 
 # Capistranoのログの表示に利用する
-set :application, first-35242
+set :application, 'first-35242'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  https://github.com/putaro1222/first-35242.git
+set :repo_url, 'https://github.com/putaro1222/first-35242.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, 2.6.5 #カリキュラム通りに進めた場合、’2.6.5’ です
+set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -60,4 +60,4 @@
 #     # password: "please use keys"
 #   }
 
-server 54.248.13.155, user: 'ec2-user', roles: %w{app db web}
+server '54.248.13.155', user: 'ec2-user', roles: %w{app db web}
